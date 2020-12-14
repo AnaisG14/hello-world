d61b1102281db93b948b62608ddf41e2c31110a4
Anais Gatard <gatard.anais@gmail.com>
2020-12-14 08:38:12 +0100

    ajout du cp au formulaire

--- hello.py
+++ hello.py
@@ -4,3 +4,6 @@ print("We are the 14th of December of 2020")
 name = input("Entrez votre prénom: ")
 print("Bonjour {}, voici une première modif".format(name))
 
+cp = input("Entrez votre code postal: ")
+print(cp)
+
